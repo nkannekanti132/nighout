c42c8634c61d3b407eb451b988a09f192346c657
Kannekanti Nikhil <98975637+nkannekanti132@users.noreply.github.com>
2024-08-30 12:09:21 -0500

    Update urls.py

--- nightout/urls.py
+++ nightout/urls.py
@@ -19,9 +19,8 @@ from django.urls import path
 from gAuth.views import google_login
 
 urlpatterns = [
-    path('api/', include('gAuth.urls')),  # Include aAuth URLs
+    path('', include('gAuth.urls')),  # Include aAuth URLs
     path('api/', include('places.urls')), # Include places URLs
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('events.urls')),
-    path('/', google_login, name='login'),  
 ]
